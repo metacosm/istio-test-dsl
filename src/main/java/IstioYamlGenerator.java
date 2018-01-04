cbba59c160a4a9459774009c5eba882553c21a94
Chris Laprun <metacosm@gmail.com>
2018-01-04 11:30:47 +0100

    Create RouteRule in code.

--- src/main/java/IstioYamlGenerator.java
+++ src/main/java/IstioYamlGenerator.java
@@ -1,31 +1,35 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.api.model.ServiceList;
+import io.fabric8.kubernetes.api.model.KubernetesResourceList;
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.api.model.apiextensions.CustomResourceDefinition;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
 import me.snowdrop.istio.api.model.v1.routing.DestinationWeight;
 import me.snowdrop.istio.api.model.v1.routing.DestinationWeightBuilder;
+import me.snowdrop.istio.api.model.v1.routing.DoneableRouteRule;
 import me.snowdrop.istio.api.model.v1.routing.RouteRule;
 import me.snowdrop.istio.api.model.v1.routing.RouteRuleBuilder;
 
 public class IstioYamlGenerator {
 
-    private static final String CLUSTER = "https://192.168.64.35:8443";
+    private static final String CLUSTER = "https://192.168.64.7:8443";
 
     public static void main(String[] args) throws JsonProcessingException {
 
         Config config = new ConfigBuilder()
-                              .withMasterUrl(CLUSTER)
-                              .withUsername("admin")
-                              .withPassword("admin")
-                              .build();
+                .withMasterUrl(CLUSTER)
+                .withUsername("admin")
+                .withPassword("admin")
+                .build();
         OpenShiftClient client = new DefaultOpenShiftClient(config);
-        ServiceList istioServices = client.services().inNamespace("demo-istio").list();
-        System.out.println("Services : ");
+
+        final CustomResourceDefinition crd = client.customResourceDefinitions().withName("routerules.config.istio.io").get();
+        System.out.println("crd = " + crd);
+
 
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
         // for(Service item : istioServices.getItems()){
@@ -36,13 +40,19 @@ public class IstioYamlGenerator {
                 .withWeight(100).build();
 
         RouteRule routeRule = new RouteRuleBuilder()
+                .withMetadata(new ObjectMetaBuilder().withGenerateName("my-rule").build())
                 .withNewDestination()
-                  .withName("greeting-service")
+                .withName("greeting-service")
                 .withNamespace("demo-istio") //optional
                 .endDestination()
                 .withRoute(destinationWeight)
                 .build();
 
-        System.out.println("Rule : " + mapper.writeValueAsString(routeRule));
+
+        final RouteRule generated = client.customResource(crd, RouteRule.class, KubernetesResourceList.class, DoneableRouteRule.class)
+                .inNamespace("istio-system")
+                .create(routeRule);
+
+        System.out.println("Generated Rule : " + mapper.writeValueAsString(generated));
     }
 }
